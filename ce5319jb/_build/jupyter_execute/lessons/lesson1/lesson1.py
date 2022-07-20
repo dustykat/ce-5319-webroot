c70a522d20b2e79823d7731521b20fa35048a086
theodore cleveland <ted.cleveland@gmail.com>
2022-07-20 21:53:06 +0000

    initial jupyterbook

---
+++ ce5319jb/_build/jupyter_execute/lessons/lesson1/lesson1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # CE 5319 Machine Learning for Civil Engineers
+
+# ## Why study machine learning?
+# 
+# The availability of big data is transforming Civil Engineering profession. Data-driven tools and
+# algorithms have the promise to model nonlinear civil engineering phenomena and extract
+# information that is not possible through traditional modeling methods. Machine learning is a fast-
+# growing field and its use in civil engineering will likely become routine in the next few years. The
+# primary objective of this course is to provide a theoretical introduction and exposure to ML
+# applications in Civil Engineering.
+
+# ## Computational Environment
+# 
+# The course itself is mathematically oriented and will require developing scripts (computer programs).  The default computational environment is a Jupyter Notebook running an iPython kernel.
+# 
+# :::{note}
+# In fact this on-line document is a collection of Jupyter Notebooks rendered using a program called Sphinx that converts the notebooks into a website (which you are now accessing).  For me the author its cool because I just make notebooks and bind them at my leisure (there are some nuances to get figures into the books and embedding code - but its not too hard to do so.  Jupyter as a literate programming environment is quite useful even outside of the University.
+# :::
+# 
+# ### Why Python?
+# A skilled user can install an R kernel into Jupyter and run R scripts.  I tend to use python because I also teach undergraduate programming in python and don't want to confuse myself.  I am literate in R, but prefer python slightly.  So we will default to python unless something is way easier in R (and will probably still do a mixed language call in that instance!).
+# 
+# 
+# No prior experience with Python or R are required, but familiarity with programming concepts covered in ENGR 1330: Computational Thinking with Data Science and in CE 5315: Probabilistic Methods for Engineers is expected. Students are strongly encouraged to take CE 5310 Numerical Methods in Engineering prior to this
+# course.
+# 
+# The two computational environments can be downloaded and installed from
+# 
+# |Software Title |Internet Source Link|
+# |---|---|
+# |Anaconda: A modeling environment that integrates Jupyter and Python |[https://www.anaconda.com/](https://www.anaconda.com/)|
+# |R statistical and programming environment|[https://cran.r-project.org/](https://cran.r-project.org/)|
+# 
+# 
+# 
+
+# In[ ]:
+
+
+
+
