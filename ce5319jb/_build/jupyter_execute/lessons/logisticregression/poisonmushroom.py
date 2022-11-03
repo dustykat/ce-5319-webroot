b07732d5023e37dc48f3545a40cbbf61696fa5b1
theodore cleveland <ted.cleveland@gmail.com>
2022-11-03 19:52:38 +0000

    poison and poisson

--- ce5319jb/_build/jupyter_execute/lessons/logisticregression/poisonmushroom.py
+++ ce5319jb/_build/jupyter_execute/lessons/logisticregression/poisonmushroom.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Poison Mushroom Model
+# # Multiple Logistic Regression (Poison Mushrooms)
 
 # In[1]:
 
 
-attributes=['''cap-shape','cap-surface','cap-color','bruises','odor','gill-attachment','gill-spacing','gill-size','gill-color','stalk-shape','stalk-root','stalk-surface-above-ring','stalk-surface-below-ring','stalk-color-above-ring','stalk-color-below-ring','veil-type','veil-color','ring-number','ring-type','spore-print-color','population','habitat']
+attributes=['poison-class','cap-shape','cap-surface','cap-color','bruises','odor','gill-attachment','gill-spacing','gill-size','gill-color','stalk-shape','stalk-root','stalk-surface-above-ring','stalk-surface-below-ring','stalk-color-above-ring','stalk-color-below-ring','veil-type','veil-color','ring-number','ring-type','spore-print-color','population','habitat']
 
 
 # In[ ]:
