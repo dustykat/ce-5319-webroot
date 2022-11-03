89a94cc8d1ceeffcc83a6600c76bf60258b83533
theodore cleveland <ted.cleveland@gmail.com>
2022-11-03 23:46:27 +0000

    vlear book

--- ce5319jb/_build/jupyter_execute/lessons/logisticregression/poisonmushroom.py
+++ ce5319jb/_build/jupyter_execute/lessons/logisticregression/poisonmushroom.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Multiple Logistic Regression (Poison Mushrooms)
+# # Poison Mushrooms 
+# 
+# An application example of multiple logistic regression.
+# 
+# Here we will have to encode the categories, then build the model.
 
 # In[1]:
 
@@ -172,31 +176,186 @@ def c9(stringvalue):
     if ncode:
         raise Exception("Encoding failed in c9 missing data maybe?")
     return(c9)
-
 ######################################################################
-def c23(stringvalue):
+def c10(stringvalue):
+#stalk-shape:enlarging=e,tapering=t
+    ncode=True
+    alphabet=['e','t']
+    for i in range(len(alphabet)):
+        if stringvalue == alphabet[i]:
+            c10=i
+            ncode=False
+    if ncode:
+        raise Exception("Encoding failed in c10 missing data maybe?")
+    return(c10)
+######################################################################
+def c11(stringvalue):
+#stalk-root: bulbous=b,club=c,cup=u,equal=e,rhizomorphs=z,rooted=r,missing=?
+    ncode=True
+    alphabet=['b','c','u','e','z','r','?'] # set missing to zero position
+    for i in range(len(alphabet)):
+        if stringvalue == alphabet[i]:
+            c11=i
+            ncode=False
+    if ncode:
+        raise Exception("Encoding failed in c11 missing data maybe?")
+    return(c11)
+######################################################################
+def c12(stringvalue):
+#stalk-surface-above-ring: fibrous=f,scaly=y,silky=k,smooth=s
+    ncode=True
+    alphabet=['f','y','k','s','?'] # set missing to zero position
+    for i in range(len(alphabet)):
+        if stringvalue == alphabet[i]:
+            c12=i
+            ncode=False
+    if ncode:
+        raise Exception("Encoding failed in c12 missing data maybe?")
+    return(c12)
+######################################################################
+def c13(stringvalue):
+#stalk-surface-below-ring: fibrous=f,scaly=y,silky=k,smooth=s
+    ncode=True
+    alphabet=['f','y','k','s','?'] # set missing to zero position
+    for i in range(len(alphabet)):
+        if stringvalue == alphabet[i]:
+            c13=i
+            ncode=False
+    if ncode:
+        raise Exception("Encoding failed in c13 missing data maybe?")
+    return(c13)
+######################################################################
+def c14(stringvalue):
+#stalk-color-above-ring:   brown=n,buff=b,cinnamon=c,gray=g,orange=o,pink=p,red=e,white=w,yellow=y
+    ncode=True
+    alphabet=['n','b','c','g','o','p','e','w','y'] # set missing to zero position
+    for i in range(len(alphabet)):
+        if stringvalue == alphabet[i]:
+            c14=i
+            ncode=False
+    if ncode:
+        raise Exception("Encoding failed in c14 missing data maybe?")
+    return(c14)
+######################################################################
+def c15(stringvalue):
+#stalk-color-below-ring:   brown=n,buff=b,cinnamon=c,gray=g,orange=o,pink=p,red=e,white=w,yellow=y
+    ncode=True
+    alphabet=['n','b','c','g','o','p','e','w','y'] # set missing to zero position
+    for i in range(len(alphabet)):
+        if stringvalue == alphabet[i]:
+            c15=i
+            ncode=False
+    if ncode:
+        raise Exception("Encoding failed in c15 missing data maybe?")
+    return(c15)
+######################################################################
+def c16(stringvalue):
+#veil-type:                partial=p,universal=u
+    ncode=True
+    alphabet=['p','u'] # 
+    for i in range(len(alphabet)):
+        if stringvalue == alphabet[i]:
+            c16=i
+            ncode=False
+    if ncode:
+        raise Exception("Encoding failed in c16 missing data maybe?")
+    return(c16)
+######################################################################
+def c17(stringvalue):
+#veil-color:               brown=n,orange=o,white=w,yellow=y
+    ncode=True
+    alphabet=['n','o','w','y'] # 
+    for i in range(len(alphabet)):
+        if stringvalue == alphabet[i]:
+            c17=i
+            ncode=False
+    if ncode:
+        raise Exception("Encoding failed in c17 missing data maybe?")
+    return(c17)
+######################################################################
+def c18(stringvalue):
+#ring-number:              none=n,one=o,two=t
+    ncode=True
+    alphabet=['n','o','t'] # 
+    for i in range(len(alphabet)):
+        if stringvalue == alphabet[i]:
+            c18=i
+            ncode=False
+    if ncode:
+        raise Exception("Encoding failed in c18 missing data maybe?")
+    return(c18)
+######################################################################
+def c19(stringvalue):
+#ring-type:  cobwebby=c,evanescent=e,flaring=f,large=l,none=n,pendant=p,sheathing=s,zone=z
+    ncode=True
+    alphabet=['c','e','f','l','n','p','s','z'] # 
+    for i in range(len(alphabet)):
+        if stringvalue == alphabet[i]:
+            c19=i
+            ncode=False
+    if ncode:
+        raise Exception("Encoding failed in c19 missing data maybe?")
+    return(c19)
+######################################################################
+def c20(stringvalue):
+#spore-print-color:        black=k,brown=n,buff=b,chocolate=h,green=r,orange=o,purple=u,white=w,yellow=y
+    ncode=True
+    alphabet=['k','n','b','h','r','o','u','w','y','?'] # 
+    for i in range(len(alphabet)):
+        if stringvalue == alphabet[i]:
+            c20=i
+            ncode=False
+    if ncode:
+        raise Exception("Encoding failed in c20 missing data maybe?")
+    return(c20)
+######################################################################
+def c21(stringvalue):
+#population:abundant=a,clustered=c,numerous=n,scattered=s,several=v,solitary=y
+    ncode=True
+    alphabet=['a','c','n','s','v','y','?'] # 
+    for i in range(len(alphabet)):
+        if stringvalue == alphabet[i]:
+            c21=i
+            ncode=False
+    if ncode:
+        raise Exception("Encoding failed in c21 missing data maybe?")
+    return(c21)
+######################################################################
+def c22(stringvalue):
 # habitat: grasses=g,leaves=l,meadows=m,paths=p,urban=u,waste=w,woods=d   
     ncode=True
     alphabet=['g','l','m','p','u','w','d']
     for i in range(len(alphabet)):
         if stringvalue == alphabet[i]:
-            c23=i
+            c22=i
             ncode=False
     if ncode:
-        raise Exception("Encoding failed in c23 missing data maybe?")
-    return(c23)
+        raise Exception("Encoding failed in c22 missing data maybe?")
+    return(c22)
 
-interim['poison-class']    = mymushroom['poison-class'].apply(p0)
-interim['cap-shape']       = mymushroom['cap-shape'].apply(c1)
-interim['cap-surface']     = mymushroom['cap-surface'].apply(c2)
-interim['cap-color']       = mymushroom['cap-color'].apply(c3)
-interim['bruises']         = mymushroom['bruises'].apply(c4)
-interim['odor']            = mymushroom['odor'].apply(c5)
-interim['gill-attachment'] = mymushroom['gill-attachment'].apply(c6)
-interim['gill-spacing']    = mymushroom['gill-spacing'].apply(c7)
-interim['gill-size']       = mymushroom['gill-size'].apply(c8)
-interim['gill-color']      = mymushroom['gill-color'].apply(c9)
-interim['habitat']         = mymushroom['habitat'].apply(c23)
+interim['poison-class']            = mymushroom['poison-class'].apply(p0)
+interim['cap-shape']               = mymushroom['cap-shape'].apply(c1)
+interim['cap-surface']             = mymushroom['cap-surface'].apply(c2)
+interim['cap-color']               = mymushroom['cap-color'].apply(c3)
+interim['bruises']                 = mymushroom['bruises'].apply(c4)
+interim['odor']                    = mymushroom['odor'].apply(c5)
+interim['gill-attachment']         = mymushroom['gill-attachment'].apply(c6)
+interim['gill-spacing']            = mymushroom['gill-spacing'].apply(c7)
+interim['gill-size']               = mymushroom['gill-size'].apply(c8)
+interim['gill-color']              = mymushroom['gill-color'].apply(c9)
+interim['stalk-shape']             = mymushroom['stalk-shape'].apply(c10)
+interim['stalk-root']              = mymushroom['stalk-root'].apply(c11)
+interim['stalk-surface-above-ring']= mymushroom['stalk-surface-above-ring'].apply(c12)
+interim['stalk-surface-below-ring']= mymushroom['stalk-surface-below-ring'].apply(c13)
+interim['stalk-color-above-ring']  = mymushroom['stalk-color-above-ring'].apply(c14)
+interim['stalk-color-below-ring']  = mymushroom['stalk-color-below-ring'].apply(c15)
+interim['veil-type']               = mymushroom['veil-type'].apply(c16)
+interim['veil-color']              = mymushroom['veil-color'].apply(c17)
+interim['ring-number']             = mymushroom['ring-number'].apply(c18)
+interim['ring-type']               = mymushroom['ring-type'].apply(c19)
+interim['spore-print-color']       = mymushroom['spore-print-color'].apply(c20)
+interim['population']              = mymushroom['population'].apply(c21)
+interim['habitat']                 = mymushroom['habitat'].apply(c22)
 
 
 # In[7]:
@@ -209,7 +368,9 @@ interim.head()
 
 
 #split dataset in features and target variable
-feature_cols = ['cap-shape', 'cap-surface','cap-color','bruises','odor','gill-attachment','gill-size','gill-color','habitat']
+#feature_cols = ['cap-shape', 'cap-surface','cap-color','bruises','odor','gill-attachment','gill-size','gill-color','habitat']
+feature_cols = attributes[1:]
+#feature_cols = attributes[1:16]+attributes[17:]
 X = interim[feature_cols] # Features
 y = interim['poison-class'] # Target variable
 # split X and y into training and testing sets
@@ -220,12 +381,18 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0
 # In[9]:
 
 
+feature_cols[16]
+
+
+# In[10]:
+
+
 # import the class
 from sklearn.linear_model import LogisticRegression
 
 # instantiate the model (using the default parameters)
 #logreg = LogisticRegression()
-logreg = LogisticRegression()
+logreg = LogisticRegression(max_iter=1000)
 # fit the model with data
 logreg.fit(X_train,y_train)
 
@@ -233,7 +400,7 @@ logreg.fit(X_train,y_train)
 y_pred=logreg.predict(X_test)
 
 
-# In[10]:
+# In[11]:
 
 
 print(logreg.intercept_[0])
@@ -241,7 +408,7 @@ print(logreg.coef_)
 #y.head()
 
 
-# In[11]:
+# In[12]:
 
 
 # import the metrics class
@@ -264,12 +431,6 @@ plt.tight_layout()
 plt.title('Confusion matrix', y=1.1)
 plt.ylabel('Predicted label')
 plt.xlabel('Actual label');
-
-
-# In[88]:
-
-
-
 
 
 # In[ ]:
