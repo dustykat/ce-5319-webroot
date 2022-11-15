c2e421a73b3d228317895161902676d83320987f
theodore cleveland <ted.cleveland@gmail.com>
2022-11-15 20:46:32 +0000

    fix hw

--- ce5319jb/_build/jupyter_execute/lessons/logisticregression/TexasNBI_database.py
+++ ce5319jb/_build/jupyter_execute/lessons/logisticregression/TexasNBI_database.py
@@ -8,6 +8,8 @@
 # In[1]:
 
 
+import warnings
+warnings.filterwarnings('ignore')
 # Playing with Texas Data
 local_file_name='2021TexasNBIData.csv'
 import pandas as pd
@@ -19,21 +21,24 @@ texasdb = pd.read_csv(local_file_name)
 
 
 # Verify the Structure
-texasdb.head()
+texasdb.describe()
 
+
+# Extract just culverts send to different object name (its probably just a [HASH](https://www.tutorialspoint.com/data_structures_algorithms/hash_data_structure.htm)
+# 
+# We name our structure `txculv` (or whatever you want)
+# 
 
 # In[3]:
 
 
-# Rename The Index
-#texasdb = texasdb.rename_axis('Index')
-#texasdb.head()
+txculv = texasdb.loc[texasdb['STRUCTURE_TYPE_043B'] == 19]
 
 
 # In[4]:
 
 
-txculv = texasdb.loc[texasdb['STRUCTURE_TYPE_043B'] == 19]
+txculv.describe()
 
 
 # In[5]:
@@ -45,7 +50,8 @@ list(texasdb)
 # In[6]:
 
 
-txculv = texasdb[['CULVERT_COND_062','YEAR_BUILT_027','ADT_029','DESIGN_LOAD_031','OPERATING_RATING_064','STRUCTURAL_EVAL_067']].copy()
+txculv = txculv[['CULVERT_COND_062','YEAR_BUILT_027','ADT_029','DESIGN_LOAD_031','OPERATING_RATING_064','STRUCTURAL_EVAL_067',
+                  'PERCENT_ADT_TRUCK_109','BRIDGE_CONDITION','YEAR_RECONSTRUCTED_106']].copy()
 
 
 # In[7]:
@@ -57,34 +63,50 @@ txculv.head()
 # In[8]:
 
 
-age = 2022 - txculv['YEAR_BUILT_027'] # age of culvert, surrogate for service life
+txculv.describe()
 
 
 # In[9]:
+
+
+age = 2022 - txculv['YEAR_BUILT_027'] # age of culvert, surrogate for service life
+
+
+# In[10]:
 
 
 drat = txculv['OPERATING_RATING_064'].fillna(1) # operating rating (in tons), nan is 1
 drat = pd.to_numeric(drat) # coerce to numeric
 
 
-# In[10]:
+# In[11]:
 
 
 steval = txculv['STRUCTURAL_EVAL_067'].fillna(1).replace('*','1') # structural eval, nan is 1 (not in original NBI coding tables)
 steval = pd.to_numeric(steval) # coerce to numeric
 
 
-# In[11]:
+# In[12]:
 
 
 adt =  txculv['ADT_029'].fillna(1) # 
 adt = pd.to_numeric(adt) # coerce to numeric
 
 
-# In[12]:
+# In[13]:
 
 
-frame = { 'AGE': age, 'DRAT': drat, "ADT" :adt, "STEVAL":steval}
+pctrk =  txculv['PERCENT_ADT_TRUCK_109'].fillna(txculv['PERCENT_ADT_TRUCK_109'].mean()) # 
+pctrk = pd.to_numeric(pctrk) # coerce to numeric
+
+
+#df_filled2 = df.fillna(df.mean())
+
+
+# In[14]:
+
+
+frame = { 'AGE': age, 'DRAT': drat, "ADT" :adt, "STEVAL":steval, "PCTRK":pctrk}
 #frame = { 'AGE': age, 'DRAT': drat, "STEVAL":steval}
 #Creating DataFrame by passing Dictionary
 X = pd.DataFrame(frame)  # our design matrix
@@ -94,7 +116,7 @@ X = pd.DataFrame(frame)  # our design matrix
 # 
 # Prepare the target variable, from the culvert condition. The series has NaN and we convert these to a goofy string
 
-# In[13]:
+# In[15]:
 
 
 pre_target = txculv['CULVERT_COND_062'].fillna('11').replace('N','10') # Nan to 11, "N" to 10 so all codes able to be numeric
@@ -103,7 +125,7 @@ print("Type ",type(pre_target[0]))
 
 # then convert from string to numeric.
 
-# In[14]:
+# In[16]:
 
 
 pre_target = pd.to_numeric(pre_target)
@@ -112,11 +134,11 @@ print("Type ",type(pre_target[0]))
 
 # Need a function to select the two conditions
 
-# In[15]:
+# In[17]:
 
 
 def isok(value_int): # function to interpret condition rating and issue binary state 0 == fail 1 == OK
-    cut = 6
+    cut = 3
     if value_int > cut:
         isok = 1
     elif value_int <= cut:
@@ -128,19 +150,19 @@ y = pre_target.apply(isok) # our target vector
 
 # Now check that our design matrix and target vector have correct structure
 
-# In[16]:
+# In[18]:
 
 
 X.describe()
 
 
-# In[17]:
+# In[19]:
 
 
 y.describe()
 
 
-# In[18]:
+# In[20]:
 
 
 # Now we can do some machine learning
@@ -149,7 +171,7 @@ from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0)
 
 
-# In[19]:
+# In[21]:
 
 
 # import the class
@@ -165,7 +187,7 @@ logreg.fit(X_train,y_train)
 y_pred=logreg.predict(X_test)
 
 
-# In[20]:
+# In[22]:
 
 
 print(logreg.intercept_[0])
@@ -173,7 +195,7 @@ print(logreg.coef_)
 #y.head()
 
 
-# In[21]:
+# In[23]:
 
 
 # import the metrics class
@@ -198,17 +220,34 @@ plt.ylabel('Predicted label')
 plt.xlabel('Actual label');
 
 
-# In[22]:
+# In[24]:
 
 
-yrbld = txculv['YEAR_BUILT_027']
-type(yrbld[0])
+# import the class
+from sklearn.linear_model import PoissonRegressor
+
+# instantiate the model (using the default parameters)
+#logreg = LogisticRegression()
+posreg = PoissonRegressor()
+# fit the model with data
+posreg.fit(X_train,y_train)
+
+#
+y_pred=posreg.predict(X_test)
 
 
-# In[23]:
+# In[25]:
 
 
-#pre_target = txculv['CULVERT_COND_062']
+print(posreg.intercept_)
+print(posreg.coef_)
+#y.head()
+
+
+# In[26]:
+
+
+y_pred
 
 
 # Structurally Deficient (SD): This term was previously defined in https://www.fhwa.dot.gov/bridge/0650dsup.cfm as having a condition rating of 4 or less for Item 58 (Deck), Item 59 (Superstructure), Item 60 (Substructure), or Item 62 (Culvert), OR having an appraisal rating of 2 or less for Item 67 (Structural Condition) or Item 71 (Waterway Adequacy) Beginning with the 2018 data archive, this term will be defined in accordance with the Pavement and Bridge Condition Performance Measures final rule, published in January of 2017, as a classification given to a bridge which has any component [Item 58, 59, 60, or 62] in Poor or worse condition [code of 4 or less].
@@ -220,7 +259,7 @@ type(yrbld[0])
 # metric tons of the entire vehicle measured to the nearest tenth of a
 # metric ton (with an assumed decimal point).
 
-# In[24]:
+# In[27]:
 
 
 #type(pre_target[0])
@@ -232,7 +271,7 @@ type(yrbld[0])
 
 # Now make a proper target
 
-# In[25]:
+# In[28]:
 
 
 #target
