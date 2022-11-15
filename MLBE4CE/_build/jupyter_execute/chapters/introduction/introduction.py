f8d3308e11f64e1394857fed94d3bca5fea7acd1
theodore cleveland <ted.cleveland@gmail.com>
2022-11-15 23:16:51 +0000

    fix hw

--- MLBE4CE/_build/jupyter_execute/chapters/introduction/introduction.py
+++ MLBE4CE/_build/jupyter_execute/chapters/introduction/introduction.py
@@ -2,3 +2,13 @@
 # coding: utf-8
 
 # # Introduction
+# 
+# ## References
+# 
+# [The One Hundred Page Machine Learning Book, Burkov (2019)](http://ema.cri-info.cm/wp-content/uploads/2019/07/2019BurkovTheHundred-pageMachineLearning.pdf)
+
+# In[ ]:
+
+
+
+
