3e47455eed38216b167ece4ce41887becd769082
theodore cleveland <ted.cleveland@gmail.com>
2023-11-17 16:45:42 +0000

    add ANN to MLBE4CE

---
+++ MLBE4CE/_build/jupyter_execute/chapters/neuralnetworks/skann.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Artificial Neural Network using packages
+
+# ## References
+# 
+# 1. [How to create ANN with/without Keras](https://www.activestate.com/resources/quick-reads/how-to-create-a-neural-network-in-python-with-and-without-keras/)
+# 2. [Python AI: How to Build a Neural Network & Make Predictions](https://realpython.com/python-ai-neural-network/)
+
+# In[ ]:
+
+
+
+
